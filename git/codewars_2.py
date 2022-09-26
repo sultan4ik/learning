b41b8ec7d5eb812d6c4531f183db2ca40c219054
sultan4ik <sir.sultanbaev@yandex.ru>
2022-09-27 00:30:33 +0500

    snail - есть решение

--- git/codewars_2.py
+++ git/codewars_2.py
@@ -5,15 +5,15 @@ def snail(snail_map):
     np_array = np.array(snail_map)
     dimension = len(snail_map)
     result = np_array[0, :]
-    while result != len(snail_map)**2:
-        result.append(np_array[:, dimension - 1])
-        result.append(np_array[dimension - 1:, dimension - 1])
+    k = 0
+    # while len(result) != len(snail_map) ** 2:
+    res_1 = np_array[k:k + 1, k:dimension]
+    res_2 = np_array[k + 1:dimension, dimension - 1:dimension]
+    res_3 = np_array[dimension - 1:dimension, k:dimension - 1]
+    res_4 = np_array[k + 1:dimension - 1, k:k + 1]
+    print(res_1, res_2, res_3, res_4)
 
 
-array_1 = [[1,2,3],
-         [4,5,6],
-         [7,8,9]]
-snail(snail_map = array_1)
 array_2 = [[1,2,3],
          [8,9,4],
          [7,6,5]]
