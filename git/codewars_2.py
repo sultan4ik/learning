41c47b017cf512d35482efedb3330bf34b0c5a00
sultanbaev_a2709 <sir.sultanbaev@yandex.ru>
2022-09-27 15:19:04 +0500

    snail finished

--- git/codewars_2.py
+++ git/codewars_2.py
@@ -3,18 +3,25 @@ import numpy as np
 
 def snail(snail_map):
     np_array = np.array(snail_map)
-    dimension = len(snail_map)
-    result = np_array[0, :]
+    dimension = np_array.shape[1]
+    result = []
     k = 0
-    # while len(result) != len(snail_map) ** 2:
-    res_1 = np_array[k:k + 1, k:dimension]
-    res_2 = np_array[k + 1:dimension, dimension - 1:dimension]
-    res_3 = np_array[dimension - 1:dimension, k:dimension - 1]
-    res_4 = np_array[k + 1:dimension - 1, k:k + 1]
-    print(res_1, res_2, res_3, res_4)
+    while dimension != 0:
+        result.extend(np_array[k:k + 1, k:dimension].tolist()[0])
+        result.extend(np_array[k + 1:dimension, dimension - 1:dimension].flatten().tolist())
+        special_row = np_array[dimension - 1:dimension, k:dimension - 1].tolist()[0]
+        special_column = np_array[k + 1:dimension - 1, k:k + 1].flatten().tolist()
+        special_row.reverse()
+        special_column.reverse()
+        result.extend(special_row)
+        result.extend(special_column)
+        k += 1
+        dimension -= 1
+    print(result)
 
 
-array_2 = [[1,2,3],
-         [8,9,4],
-         [7,6,5]]
+array_2 = [[1, 2, 3, 4],
+         [12, 13, 14, 5],
+         [11, 16, 15, 6],
+           [10, 9, 8, 7]]
 snail(snail_map = array_2)
